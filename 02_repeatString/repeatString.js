43092e2d7e71052f099fe8b606adc1c2a2e89993
simp1ord <jpkbobblehead@gmail.com>
2022-03-10 21:30:32 -0800

    brain fried, trying tmmrw

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,10 @@
 const repeatString = function() {
-
+    for(i = 0; i < 3; i++){
+        let num = ('hey');
+    }
+return(num);
 };
+let string =
 
 // Do not edit below this line
 module.exports = repeatString;
