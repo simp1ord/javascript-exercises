8ffd36ca4a670e718bb0e1ee925e30042fe875e4
simp1ord <jpkbobblehead@gmail.com>
2022-03-13 18:12:30 -0700

    finsihed practice 5-7

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,27 @@
-const sumAll = function() {
-
-};
-
+const sumAll = function(int1, int2) {
+  if(int1 < 0 || int2 < 0){
+      return('ERROR');
+  } else if(typeof int1 === 'string' || typeof int2 === 'string'){
+      return('ERROR');
+  } else if(Array.isArray(int1) || Array.isArray(int2)){
+      return('ERROR');
+  } else if(int1 > int2){
+    let i = int2;
+      while(i <= int1){
+        int2 += i;
+        i++;
+      }
+    finalSum = int2 - (i-int1);
+    return(finalSum);
+  }else {
+    let i = int1;
+      while(i <= int2){
+        int1 += i;
+        i++;
+      }
+    finalSum = int1 - (i-int2);
+    return(finalSum);
+  }
+}
 // Do not edit below this line
 module.exports = sumAll;
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,22 @@
-const ftoc = function() {
-
-};
-
-const ctof = function() {
-
-};
+const ftoc = function(fahrenheit) {
+  let c_Conversion = (fahrenheit - 32) * (5/9);
+    if (c_Conversion === 0){
+      return(c_Conversion);
+    } else{
+      let finalConversion = Math.round(c_Conversion * 10) / 10;
+      return(finalConversion);
+    }
+  };
+  
+  const ctof = function(celsius) {
+    let f_Conversion = celsius * (9/5) + 32;
+    if(f_Conversion === 0){
+      return(f_Conversion);
+    }else {
+      let finalConversion2 = Math.round(f_Conversion * 10) / 10;
+      return(finalConversion2);
+    }
+  };
 
 // Do not edit below this line
 module.exports = {
