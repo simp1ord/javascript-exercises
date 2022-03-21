d74899ff925aac240a2ea1d1e8c0050b011f1bf8
Jeremiah Kemp <jpkbobblehead@gmail.com>
2022-03-20 21:56:50 -0700

    finsihed 09-palendromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,21 +1,23 @@
 const palindromes = function (palindromeStatement) {
-    let lowercase = palindromeStatement.toLowerCase();
-    const palindromeArray = lowercase.trim().split(" ");
+  let lowercase = palindromeStatement.toLowerCase();
+  const noSpecialCharacter = lowercase.replace(/[!,.\s]/g,''); 
+  console.log(noSpecialCharacter);
+    const palindromeArray = noSpecialCharacter.trim().split(" ");
     console.log(palindromeArray);
-    if(palindromeArray.length === 0){
-        
-    }else{
+    if(palindromeArray.length === 1){
       let passedString = palindromeArray.toString();
       let arrayCharacter = [... passedString];
-      let punctuation = arrayCharacter.pop()
       let reverse = arrayCharacter.reverse().join('');
-      if((reverse + punctuation) == lowercase){
+      if(reverse == noSpecialCharacter){
         return(true);
       }else{
         return(false);
-      }
+      } 
+    }else{
+      
 }
 }
+
 
 // Do not edit below this line
 module.exports = palindromes;
