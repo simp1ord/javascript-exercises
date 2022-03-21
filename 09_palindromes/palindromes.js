9991aaed23fdd49c6b20769fd79e890d0fb7c79f
Jeremiah Kemp <jpkbobblehead@gmail.com>
2022-03-21 10:54:53 -0700

    stuff'

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,22 +1,17 @@
 const palindromes = function (palindromeStatement) {
   let lowercase = palindromeStatement.toLowerCase();
   const noSpecialCharacter = lowercase.replace(/[!,.\s]/g,''); 
-  console.log(noSpecialCharacter);
-    const palindromeArray = noSpecialCharacter.trim().split(" ");
-    console.log(palindromeArray);
-    if(palindromeArray.length === 1){
-      let passedString = palindromeArray.toString();
-      let arrayCharacter = [... passedString];
-      let reverse = arrayCharacter.reverse().join('');
+  const palindromeArray = noSpecialCharacter.trim().split(" ");
+  let passedString = palindromeArray.toString();
+  let arrayCharacter = [... passedString];
+  let reverse = arrayCharacter.reverse().join('');
       if(reverse == noSpecialCharacter){
         return(true);
       }else{
         return(false);
       } 
-    }else{
-      
 }
-}
+
 
 
 // Do not edit below this line
