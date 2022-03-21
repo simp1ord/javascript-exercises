c2846fbd69407af26aff29d8194c11dd839f0a91
Jeremiah Kemp <jpkbobblehead@gmail.com>
2022-03-20 19:30:15 -0700

    stuck on 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,62 @@
-const add = function() {
-	
+const add = function(addOne, addTwo) {
+	let sum = addOne + addTwo;
+  return(sum);
 };
 
-const subtract = function() {
-	
+const subtract = function(subOne, subTwo) {
+	let remainder = subOne - subTwo;
+  return(remainder);
 };
 
-const sum = function() {
-	
+const sum = function(numbList) {
+let sumOfNumbs = 0;
+  const array = Array.from(numbList);
+  if (array.length < 1) {
+    return(sumOfNumbs);
+  }else{
+    array.forEach(numbers => {
+      sumOfNumbs += numbers;
+    });
+  return(sumOfNumbs);
+  }
 };
 
-const multiply = function() {
-
+const multiply = function(numberList) {
+  const multiplyArray = Array.from(numberList);
+  let multible = numberList[0];
+    if(multiplyArray.length < 1){
+      return(multible);
+    } else{
+      multiplyArray.forEach(multiply => {
+        if (multiply === numberList[0]){
+          //skip number
+        }else{
+          multible *= multiply;
+        }
+      });
+      return(multible);
+    }
 };
 
-const power = function() {
-	
+const power = function(powerNumber, power) {
+let powerSum = powerNumber
+let i = 0;
+while(i < power) {
+    i === 0 ? i++ : (powerSum *= powerNumber, i++);
+}
+return(powerSum);
 };
 
-const factorial = function() {
-	
+const factorial = function(factorial) {
+let factorialMultiply = factorial;
+  if(factorialMultiply === 0){
+    return(1);
+  }else {
+    for(i = factorial; i >= 1; i--){
+      i === factorial ? '' : factorialMultiply *= i;
+    }
+    return(factorialMultiply);
+  }
 };
 
 // Do not edit below this line
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,21 @@
-const palindromes = function () {
-
-};
+const palindromes = function (palindromeStatement) {
+    let lowercase = palindromeStatement.toLowerCase();
+    const palindromeArray = lowercase.trim().split(" ");
+    console.log(palindromeArray);
+    if(palindromeArray.length === 0){
+        
+    }else{
+      let passedString = palindromeArray.toString();
+      let arrayCharacter = [... passedString];
+      let punctuation = arrayCharacter.pop()
+      let reverse = arrayCharacter.reverse().join('');
+      if((reverse + punctuation) == lowercase){
+        return(true);
+      }else{
+        return(false);
+      }
+}
+}
 
 // Do not edit below this line
 module.exports = palindromes;
